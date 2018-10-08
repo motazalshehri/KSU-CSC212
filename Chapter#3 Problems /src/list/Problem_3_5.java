2048409d3168114901f0e49f44459138739b0e65
ENG.NAIF ALSHEHRI <IT676@NAIFs-MacBook-Pro-2.local>
2018-10-08 21:53:27 +0300

    Fixing the logical error in 3_5 problem

--- Chapter#3 Problems /src/list/Problem_3_5.java
+++ Chapter#3 Problems /src/list/Problem_3_5.java
@@ -11,7 +11,7 @@ package list;
 public class Problem_3_5 {
 
     public static void main(String[] args) {
-        //List<String> list = new ArrayList<String>(10);
+        //List<Integer> list = new ArrayList<Integer>(10);
         //or
         List<Integer> list = new LinkedList<Integer>();
 
@@ -20,10 +20,11 @@ public class Problem_3_5 {
         list.insert(3);
         list.insert(4);
         list.insert(2);
-        list.insert(5);
+        list.insert(3);
+        list.insert(3);
         list.insert(3);
 
-        System.out.println(mfe(list));
+        System.out.println("The Most Frequent Element is : " + mfe(list));
 
     }
 
@@ -39,17 +40,22 @@ public class Problem_3_5 {
             counter++;
         }
 
-        int currentMoveCounter = 0;
-
-        counter++;//for the last
+        counter++;//for the last item
         int frequency = 0;
         T mostFrquentElement = null;
         int max = 0;
-
+        int currentMoveCounter = 0;
         l.findFirst();
+
         for (int i = 0; i < counter;) {
 
             T item = l.retrieve();
+            /*
+              To show Which element is being testing , print it
+              --> for testing only
+             */
+            System.out.println("Current Element Testing : " + item); //demo
+
             while (!l.last()) {
 
                 if (item.equals(l.retrieve())) {
@@ -64,17 +70,30 @@ public class Problem_3_5 {
 
             if (frequency > max) {
                 max = frequency;
+
+                mostFrquentElement = item;
+
             }
 
-            mostFrquentElement = item;
+            frequency = 0;//reset
+
             i++;
             l.findFirst();
+
             while (currentMoveCounter < i) {
                 l.findNext();
+
                 currentMoveCounter++;
             }
+            currentMoveCounter = 0;//reset the moves counter 
 
         }
+
+        /*
+         To show The Max Frequent Times then print  max
+         --> for testing only
+         */
+        System.out.println("Max Frequent Times: " + max + "  by the Item : " + mostFrquentElement); //demo
 
         return mostFrquentElement;
     }
